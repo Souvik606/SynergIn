00a15c993c367a942d0eb2e35912751bd87b7085
Souvik606 <mukherjeesouvik606@gmail.com>
2025-03-06 15:00:43 +0530

    enabled cors in backend and frontend setup done along with tanstack query and axios

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: "http://localhost:5173",
   credentials: true,
-}));
+  methods: ["GET", "POST", "PATCH", "DELETE"]
+})
+);
 
 app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({ extended: true }));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Layout from "./components/layout/Layout.jsx";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+
+function App() {
+  return <Layout>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+    </Routes>
+  </Layout>
+}
+
+export default App
