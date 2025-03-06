5d851af4234a2e7a19fbeb9a666ae8109f350f56
Souvik606 <mukherjeesouvik606@gmail.com>
2025-03-07 00:26:06 +0530

    Designed the login page and added the login functionalities

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,5 +1,38 @@
-const LoginPage = () => {
-  return <div>Login page</div>
-}
+import { Link } from "react-router-dom";
+import LoginForm from "../../components/auth/LoginForm";
 
+const LoginPage = () => {
+  return (
+    <div className='min-h-[85vh] flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <img className='mx-auto h-20 w-auto' src='/logo.svg' alt='LinkedIn' />
+        <h2 className=' text-center text-3xl font-bold text-gray-900 pt-4'>Sign in to your account</h2>
+      </div>
+
+      <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md shadow-md'>
+        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+          <LoginForm />
+          <div className='mt-6'>
+            <div className='relative'>
+              <div className='absolute inset-0 flex items-center'>
+                <div className='w-full border-t border-gray-300'></div>
+              </div>
+              <div className='relative flex justify-center text-sm'>
+                <span className='px-2 bg-white text-gray-500 text-lg font-semibold'>New to SynergIn?</span>
+              </div>
+            </div>
+            <div className='mt-6'>
+              <Link
+                to='/signup'
+                className='text-xl font-bold w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-600 bg-white hover:bg-gray-50'
+              >
+                Join now
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default LoginPage;
